54f1e5cbc70e9e1fd8abc1d4023c8ed774279162
nicolasxz13 <nicolasxz13@gmail.com>
2024-12-03 17:50:53 -0300

    Update authFilter.js: Rewrite filterIsAdminJWT function.

--- src/middlewares/authFilter.js
+++ src/middlewares/authFilter.js
@@ -1,42 +1,31 @@
-const { error } = require('console');
 const jwt = require('jsonwebtoken');
 
 const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, "./config/.env") });
+require("dotenv").config({path: path.resolve(__dirname, "./config/.env")});
 
-module.exports.filterIsAdminJWT = async (req, res, next) => {
-    const token = req.cookies['x-auth'];
-
-    if (!token) {
-        return res.status(401).json({ error: 'Credenciales no proporcionadas. Autenticación requerida.' });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (decoded.is_admin) {
-            next();
-        } else {
-            return res.status(403).json({ error: 'Acceso denegado. Privilegios insuficientes.' });
-        }
-    } catch (err) {
-        console.error('Error al verificar el token:', err);
-        return res.status(401).json({ error: 'Token inválido o expirado.' });
+function filterByRole(req, res, next, name) {
+    if (req.user?.roles?.some((role) => role.includes(name))) {
+        next();
+    } else {
+        return res.status(403).json({error: 'Acceso denegado. Privilegios insuficientes.'});
     }
 }
 
-module.exports.AuthenticationJWT = async (req, res, next) => {
+module.exports.filterIsAdminJWT = (req, res, next) => {
+    filterByRole(req, res, next, 'Administrator');
+};
+
+module.exports.authenticationJWT = async (req, res, next) => {
     try {
 
         const token = req.headers['authorization']?.split(' ')[1];
 
         if (!token) {
-            return res.status(401).json({ error: 'Credenciales no proporcionadas. Autenticación requerida.' });
+            return res.status(401).json({error: 'Credenciales no proporcionadas. Autenticación requerida.'});
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
         // Si el token es válido, puedes guardar la información del usuario en el objeto req para uso posterior
-        req.user = decoded; // Esto puede incluir información como el id del usuario, roles, etc.
+        req.user = jwt.verify(token, process.env.JWT_SECRET); // Esto puede incluir información como el id del usuario, roles, etc.
         next();
     } catch (err) {
         next(err);
