421ca0fead192ff7bf8ef599f284d3bf8ff80751
nicolasxz13 <nicolasxz13@gmail.com>
2024-12-09 13:12:42 -0300

    Add getAllTeacherCoursesByID

--- src/controllers/account/accountController.js
+++ src/controllers/account/accountController.js
@@ -55,7 +55,7 @@ function getUserCourses(req,res,id){
         return res.status(404).json({ message: "Not found" });
     }
 
-    const consult = 'SELECT course.title ,student_status.`name`, course. AS student_status FROM `courses`.`student_has_course` AS student INNER JOIN `courses`.`course` ON course.course_id = student.course_id INNER JOIN `courses`.`student_course_status` AS student_status ON student_status.student_course_status_id = student.status_id WHERE user_id = ?;';
+    const consult = 'SELECT course.title ,student_status.`name` AS student_status,cs.name AS `course status` FROM `courses`.`student_has_course` AS student INNER JOIN `courses`.`course` ON course.course_id = student.course_id INNER JOIN `courses`.`course_status` AS cs ON cs.course_status_id = `courses`.course .course_status_id INNER JOIN `courses`.`student_course_status` AS student_status ON student_status.student_course_status_id = student.status_id WHERE user_id = ?;';
     const connection = createConnection(); // Crea una nueva conexión
 
     try {
@@ -91,3 +91,40 @@ module.exports.getAllMyUserCourses = (req, res) => {
     const id = req.user?.id;
     getUserCourses(req,res,id);
 };
+
+module.exports.getAllTeacherCoursesByID = (req, res) => {
+    const id = Number(req.params.id);
+
+    if (!Number.isInteger(id)) {
+        return res.status(400).json({ error: "Bad Request" });
+    }
+
+    if (id === 0) {
+        return res.status(404).json({ message: "Not found" });
+    }
+
+    const consult = 'SELECT course.title ,student_status.`name` AS student_status,cs.name AS `course status` FROM `courses`.`student_has_course` AS student INNER JOIN `courses`.`course` ON course.course_id = student.course_id INNER JOIN `courses`.`course_status` AS cs ON cs.course_status_id = `courses`.course .course_status_id INNER JOIN `courses`.`student_course_status` AS student_status ON student_status.student_course_status_id = student.status_id WHERE `courses`.`course`.teacher_user_id = ?;';
+    const connection = createConnection(); // Crea una nueva conexión
+
+    try {
+        connection.query(consult, [id], (err, result) => {
+            if (err) {
+                console.error("Error ejecutando la consulta:", err.message);
+                return res.status(500).json({ error: "Error ejecutando la consulta" });
+            }
+            if (result.length === 0) {
+                return res
+                    .status(404)
+                    .json({ message: "Not found"  });
+            }
+            res.json(result);
+        });
+    } catch (err) {
+        console.error("Error en el controlador:", err.message);
+        res.status(500).json({ error: "Ocurrió un error en el servidor" });
+    } finally {
+        connection.end((err) => {
+            if (err) console.error("Error cerrando la conexión:", err.message);
+        });
+    }
+}
